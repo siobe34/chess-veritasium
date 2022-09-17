99517cd84056694c651e04330fe855ed8ea6c4ea
ibad <siobe14@hotmail.com>
2022-09-17 00:30:59 -0400

    responsive navbar with mobile menu

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,117 +1,70 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChessQueen } from '@fortawesome/free-solid-svg-icons';
-// import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faChessQueen } from '@fortawesome/free-solid-svg-icons';
+
+import Button from './Button';
 
 function Navbar() {
-    const [mobileMenu, setMobileMenu] = useState<boolean>(false);
-    const handleMenuToggle = () => {
-        if (window.innerWidth <= 786) setMobileMenu(!mobileMenu);
-    };
-    const handleMenuClose = () => {
-        if (window.innerWidth <= 786) setMobileMenu(false);
-    };
+    const location = useLocation();
+    const [showMobileMenu, setMobileMenu] = useState<boolean>(false);
+
+    useEffect(() => {
+        setMobileMenu(false);
+    }, [location]);
+
     return (
         <>
-        <nav className='flex items-center content-center justify-between flex-wrap bg-blue-500'>
-            <div className='flex items-center flex-shrink-0 text-white ml-6 mr-12'>
+        <nav className='relative flex items-center content-center justify-between bg-blue-500'>
+            <div className='flex items-center text-white ml-6 mr-12'>
                 <Link to='/'>
                     <FontAwesomeIcon className='mr-2' icon={faChessQueen} />
                     <span className='font-semibold text-xl tracking-tight'>10K-Chess</span>
                 </Link>
             </div>
-            <div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
-                <div className='text-sm lg:flex-grow'>
-                    <Link
-                        className='block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4'
-                        to='/practice'
-                    >
-                        Practice
-                    </Link>
-                    <Link
-                        className='block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4'
-                        to='/about'
-                    >
-                        About
-                    </Link>
-                </div>
+            <div className={`
+                absolute
+                ${showMobileMenu ? 'flex' : 'hidden'}
+                flex-col
+                items-center
+                gap-2
+                pb-2
+                w-full
+                bg-blue-500
+                text-sm
+                sm:static
+                sm:flex
+                sm:flex-row
+                sm:w-auto
+                sm:pb-0`}
+                style={{ top: '100%' }}
+            >
+                <Link
+                    className='text-blue-200 hover:text-white mr-4'
+                    to='/practice'
+                >
+                    Practice
+                </Link>
+                <Link
+                    className='text-blue-200 hover:text-white mr-4'
+                    to='/about'
+                >
+                    About
+                </Link>
             </div>
-            <div className='block lg:active'>
-                <button className='flex items-center px-3 py-2 border rounded text-blue-200 border-blue-400 hover:text-white hover:border-white'>
-                <svg className='fill-current h-3 w-3' viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
+            <div className='flex mr-6 sm:hidden'>
+                <Button
+                    buttonStyle='primary'
+                    customUtils='border-blue-400 hover:text-white hover:border-white'
+                    onClick={() => setMobileMenu(!showMobileMenu)}
+                >
+                    <FontAwesomeIcon icon={faBars}/>
+                </Button>
             </div>
         </nav>
         </>
     );
-}
-
-{/* <nav className={`navbar ${className}`}>
-    <Link to='/' onClick={handleMenuClose}>
-        <Logo />
-    </Link>
-        <ul className={`navigation-links-container ${mobileMenu ? 'active' : null}`}>
-            <NavLink to='/page-not-found' onClick={handleMenuClose}>
-                PageNotFound
-            </NavLink>
-        </ul>
-    <div className='mobile-menu' onClick={handleMenuToggle}>
-        <FontAwesomeIcon icon={faBars} />
-    </div>
-</nav> */}
-// const Navbar = styled(UnstyledNavbar)`
-//     position: relative;
-//     display: flex;
-//     align-items: center;
-//     justify-content: space-between;
-//     border-bottom: 1px solid black;
-//     // *** //
-//     .logo {
-//         margin-left: 1rem;
-//     }
-//     .navigation-links-container {
-//         display: flex;
-//         align-items: center;
-//         justify-content: center;
-//         gap: 2vw;
-//         list-style-type: none;
-//         margin-block-start: 0;
-//         margin-block-end: 0;
-//         margin-inline-start: 0;
-//         margin-inline-end: 0;
-//         padding-inline-start: 0;
-//     }
-//     .mobile-menu {
-//         display: none;
-//     }
-//     .mobile-menu svg {
-//         height: 1.5rem;
-//     }
-//     @media screen and (max-width: 786px) {
-//         .mobile-menu {
-//             display: inline-flex;
-//             margin-right: 1rem;
-//             cursor: pointer;
-//         }
-//         .navigation-links-container {
-//             position: absolute;
-//             top: calc(100% + 40px);
-//             left: -150%;
-//             display: flex;
-//             flex-direction: column;
-//             gap: 0;
-//             width: 100%;
-//             background-color: white;
-//             transition: left 250ms ease;
-//         }
-//         .navigation-links-container.active {
-//             display: flex;
-//             left: 0%;
-//             transition: left 250ms ease;
-//         }
-//     }
-// `;
+};
 
 export default Navbar;
