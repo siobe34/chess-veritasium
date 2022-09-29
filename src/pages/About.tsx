b04053b824f397a1097e0007ce4665c8e95795c2
ibad <siobe14@hotmail.com>
2022-09-29 15:41:50 -0400

    cleaning About page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 import Content from "../components/Content";
 import Button from "../components/Button";
@@ -10,25 +10,15 @@ import Table, { TableBody, TableRow, TableCell } from "../components/Table";
 function About() {
     return (
         <Content>
-            <h1 className='font-bold text-3xl mt-6'>About</h1>
-            <h1 className='font-bold text-2xl text-center mt-6'>Why'd you make this site?</h1>
-            <div className='flex flex-col items-center justify-center w-7/12'>
-                <p className='leading-loose'>
-                    Mainly for education purposes, I'd never worked with Tailwind before and wanted to try it out. And I was also inspired by
-                    Veritasium's video on{" "}
-                    <a className='text-blue-500 underline hover:text-blue-300' href='https://www.youtube.com/watch?v=5eW6Eagr9XA'>
-                        mastering skills
-                    </a>{" "}
-                    to become decent at chess. I'd always wondered what set apart GM's from other chess players. Time, experience, and practice set
-                    aside - there had to be other factors at play. Recognition of chess positions is surely one of those factors, though I'm certain
-                    there's plenty more. Personally, part of my shortcomings as a player is my lack of ability to recognize positions in the way other
-                    players do. So I made this site to practice that one specific aspect of chess.
-                </p>
-            </div>
             <h1 className='font-bold text-2xl text-center mt-6'>What development tools were used?</h1>
             <div className='flex flex-col items-center justify-center w-7/12'>
                 <Table>
                     <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                Deployed for free with Vercel <FontAwesomeIcon className='text-red-400' icon={faHeart} />
+                            </TableCell>
+                        </TableRow>
                         <TableRow>
                             <TableCell>React</TableCell>
                         </TableRow>
@@ -45,7 +35,7 @@ function About() {
                             <TableCell>Chess.js</TableCell>
                         </TableRow>
                         <TableRow>
-                            <TableCell>react-chessboard (npm)</TableCell>
+                            <TableCell>react-chessboard (npm package)</TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
